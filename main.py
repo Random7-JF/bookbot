47550ceecb6c7d2f46a6875a13c07025cc940ecd
Jon Fingas <jon.fingas@gmail.com>
2024-01-08 06:21:43 -0600

    count letters func

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_book_text(book_path)
     print(text)
     print(get_word_count(text))
+    letters = get_letter_count(text)
+    print(letters)
 
 
 def get_book_text(path):
@@ -10,8 +12,17 @@ def get_book_text(path):
         return f.read()
     
 def get_word_count(book:str):
-    words = book.split(" ")
+    words = book.split()
     return len(words)
+
+def get_letter_count(book:str) -> dict:
+    letterstats = dict()
+    for letter in book.lower():
+        if letter not in letterstats.keys():
+            letterstats[letter] = 1
+        else:
+            letterstats[letter] = letterstats[letter] +1
+    return letterstats
 
 
 main()
