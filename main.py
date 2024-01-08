03b813f0ed3af56ab4978cf43a1c4ad4acbfb76d
Jon Fingas <jon.fingas@gmail.com>
2024-01-08 06:11:23 -0600

    wrod count

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     print(text)
+    print(get_word_count(text))
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def get_word_count(book:str):
+    words = book.split(" ")
+    return len(words)
 
 
 main()
